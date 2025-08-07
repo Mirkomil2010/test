6160eea4cd85fcaf94098847f0aa3bf937a55d27
Mirkomil2010 <mirjalilazizov@gmail.com>
2025-08-07 17:10:13 +0500

    test

---
+++ script.js
@@ -0,0 +1,67 @@
+// 1-savol
+// function makeNegative(num) {
+//     if(num>0){
+//         console.log(-num);
+//     }else{
+//         console.log(num);
+//     }
+
+
+// }
+// makeNegative(5)
+// makeNegative(3)
+// makeNegative(0)
+
+// 2-savol
+// function evenOrOdd(num) {
+//     if(num % 2 === 0){
+//         console.log("Even");
+//     }else{
+//         console.log("Odd");
+//     }
+
+
+// }
+// evenOrOdd(2)
+// evenOrOdd(7)
+
+// 3-savol
+// function removeChar(str) {
+//     let arr = str.split('')
+//     let res = arr.slice(1, -1).join('').toString()
+//     console.log(res);
+
+
+// }
+// removeChar("hello")
+// removeChar("abc")
+
+// 4-savol
+// function solution(str) {
+//     let arr = str.split('')
+//     let res = arr.reverse().join('').toString()
+//     console.log(res);
+// }
+
+// solution("world")
+
+// 5-savol
+// function positiveSum(arr) {
+//     const num = arr.filter(num => num > 0);
+//     const sum = num.reduce((acc, cur) => acc + cur, 0);
+//     console.log(sum);
+// }
+
+// positiveSum([1, -2, 3, 4, -5])
+
+// 6-savol
+// function repeatStr(n, s) {
+//     return s.repeat(n);
+// }
+
+// console.log(repeatStr(3, "ha"));
+
+
+
+
+
